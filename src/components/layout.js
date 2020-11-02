20805e49dd144152f81df0d43277cb801b0fc482
Zuzanna Orlowska <z.orlowska1@gmail.com>
2020-11-03 00:22:22 +0100

    changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,7 +34,15 @@ const countries = ["United States", "Canada", "Afghanistan", "Albania", "Algeria
 const yesno = ["Yes", "No", "Not Sure"]
 const countries2 = ["United States", "Canada", "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and/or Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecudaor", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France, Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kosovo", "Kuwait", "Kyrgyzstan", "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfork Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia South Sandwich Islands", "South Sudan", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbarn and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States minor outlying islands", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela", "Vietnam", "Virigan Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zaire", "Zambia", "Zimbabwe"]
 
-
+function StepZero (props) {
+  return (
+    <div style={{display: !props.show ? 'none' : 'block'}}>
+      <p><strong> Hi, welcome to our questionnaire! </strong> </p>
+      <p><strong>This form consists of 8 very short steps. Thanks for your time!</strong></p>
+      <p><strong> We will keep your data anonymous! If that's ok with you, let's start.</strong></p>
+    </div>
+  )
+}
 
 function StepOne(props) {
 
@@ -191,6 +199,7 @@ function StepNine (props) {
     <div style={{display: !props.show ? 'none' : 'block'}}>
       <p><strong>Would you like to leave your email, so that you can be updated about our development? </strong> </p>
       <p> If not, please type in a "no"</p>
+      <p> We won't share your data with anyone else!</p>
       <input
       type='text'
       name='email'
@@ -302,6 +311,7 @@ function Layout() {
   return (
     <div className="App">
       <ul className="steps-numbers">
+       <li className={"steps-number " + (step === 0 && "active")}>1</li>
         <li className={"steps-number " + (step === 1 && "active")}>1</li>
         <li className={"steps-number " + (step === 2 && "active")}>2</li>
         <li className={"steps-number " + (step === 3 && "active")}>3</li>
@@ -321,6 +331,7 @@ function Layout() {
         }}
       >
         <input type="hidden" name="form-name" value="responses" />
+        <StepZero show={(step === 0)}/>
         <StepOne show={(step === 1)} handleMoods={setMoodsPicked} moodsPicked={moodsPicked}/>
         <StepTwo show={(step === 2)} moods={moodsPicked}/>
         <StepThree show={(step === 3)} color={color} handleColor={handleColorChange}/>
@@ -333,7 +344,7 @@ function Layout() {
         <StepTen show={(step === 10)}/>
 
       <div className="steps-nav">
-        {(step > 1 && step < 10) && <button type="button" onClick={moveBack}>BACK</button>}
+        {(step > 0 && step < 10) && <button type="button" onClick={moveBack}>BACK</button>}
         {step < 9 && <button type="button" onClick={moveForward}>FORWARD</button>}
         {step === 9 && <button type="submit">SUBMIT</button>}
       </div>
